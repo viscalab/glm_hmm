970e0b787c104e41059d23b4ed88da3291e9ed8e
brunopless <120565251+brunopless@users.noreply.github.com>
2023-06-06 14:46:38 +0200

    Delete R directory

--- R/plotting_one_participant_DL.R
+++
@@ -1,7 +0,0 @@
-plotting_one_participant_DL <- function(.fit, .label = "") {
-  plot_grid(plotting_psycho_one_participant(.fit), 
-            plotting_trials_one_participant_DL(.fit),
-            labels = .label,
-            label_x = .15, label_y = .95, 
-            rel_widths = c(.5, .5))
-}
