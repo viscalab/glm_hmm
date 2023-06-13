b450693903df875af7a5e518e69d437118fc71f0
danilinares <danilinares@gmail.com>
2023-06-13 22:16:19 +0200

    adding function

--- R/glm_hmm_extract_best_precision.R
+++ R/glm_hmm_extract_best_precision.R
@@ -1,37 +1,38 @@
 glm_hmm_extract_best_precision <- function(.glm_hmm) {
-  fit <- .glm_hmm %>% 
-    filter(log_lik == max(log_lik)) %>% 
-    pluck("fit") %>% 
-    pluck(1)
   
-  posterior_wins <- fit$posterior_probs %>% 
+  fit <- .glm_hmm |> 
+    filter(log_lik == max(log_lik)) |> 
+    select(fit) |> 
+    pluck("fit") |> 
+    pluck(1) 
+  
+  
+  posterior_wins <- fit$posterior_probs %>%
     group_by(trial) %>%
     filter(p == max(p)) %>%
-    select(-p) 
+    select(-p)
   
+  states_correspondence <- fit$recovered_weights |>
+    filter(coef == "V2") |>
+    arrange(desc(value), .by_group = TRUE) |>
+    mutate(ordered_state = paste0("S", row_number())) |>
+    select(state, ordered_state)
   
-  ordered_states <- order_states_glm_hmm(fit)
+  fit$recovered_weights <- fit$recovered_weights %>%
+    left_join(states_correspondence , by = "state") %>%
+    mutate(state = ordered_state) %>%
+    select(-ordered_state) %>%
+    arrange(state)
   
-  ordered_states
-
-  # states_correspondence <- ordered_states %>%
-  #   select(state, ordered_state)
-  # 
-  # fit$recovered_weights <- fit$recovered_weights %>%
-  #   left_join(states_correspondence , by = "state") %>%
-  #   mutate(state = ordered_state) %>%
-  #   select(-ordered_state) %>%
-  #   arrange(state)
-  # 
-  # fit$posterior_probs <- fit$posterior_probs %>%
-  #   left_join(states_correspondence , by = "state") %>%
-  #   mutate(state = ordered_state) %>%
-  #   select(-ordered_state)
-  # 
-  # fit$ordered_states <- ordered_states %>%
-  #   mutate(state = ordered_state) %>%
-  #   select(-ordered_state) %>%
-  #   arrange(state)
-  # 
-  # fit
+  fit$posterior_probs <- fit$posterior_probs %>%
+    left_join(states_correspondence , by = "state") %>%
+    mutate(state = ordered_state) %>%
+    select(-ordered_state)
+  
+  fit$ordered_states <- states_correspondence %>%
+    mutate(state = ordered_state) %>%
+    select(-ordered_state) %>%
+    arrange(state)
+  
+  fit
 }
